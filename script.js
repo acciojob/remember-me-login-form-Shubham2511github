d335387c305248a29adf2758479e370ce4fbea16
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-28 23:02:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var first = document.getElementById("submit");
 var second = document.getElementById("existing");
 first.addEventListener('click', submitForm);
 second.addEventListener('click', existingUser);
-function submitForm(event){
-	event.preventDefault();
+function submitForm(){
+	//event.preventDefault();
 	var username = document.getElementById('username').value;
 	var password = document.getElementById('password').value;
 	var rememberMe = document.getElementById('checkbox').checked;
